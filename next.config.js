871564548f0e72d49e2a3751e85ddddbef7de8da
ogabrielrodrigues <gabrielrodrigues409@outlook.com>
2025-12-27 13:42:47 -0300

    fix: remote patterns object

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: ["github.com", "raw.githubusercontent.com"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "github.com",
+			},
+			{
+				protocol: "https",
+				hostname: "raw.githubusercontent.com",
+			},
+		],
 	},
 };
 
