7fc11e873c7103b8b68572da9ab05122ed7d8a41
ogabrielrodrigues <gabrielrodrigues409@outlook.com>
2025-12-27 13:40:10 -0300

    fix: domains config on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['github.com', 'raw.githubusercontent.com']
-  }
-}
+	images: {
+		remotePatterns: ["github.com", "raw.githubusercontent.com"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
